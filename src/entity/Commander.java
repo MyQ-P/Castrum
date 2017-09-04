5636331c6c47abc001bf2cf0398b9b25375f4dc8
nofva <mikepiek4902@gmail.com>
2017-09-04 16:55:52 -0700

    First commit.

---
+++ src/entity/Commander.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Commander extends Unit{
+	
+	public Commander(int hp,int ac,int x,int y){
+		super(hp,ac,x,y);
+	}
+	
+}
---
+++ src/entity/Unit.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Unit {
+	
+	private int hp, ac, damagemin, damagemax, fcst, ocst, scst, wcst, x, y;
+	
+	public Unit(int hp,int ac,int x,int y){
+		this.hp = hp;
+		this.ac = ac;
+		this.x = x;
+		this.y = y;
+	}
+	
+}
